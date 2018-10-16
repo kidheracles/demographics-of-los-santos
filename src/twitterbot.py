b4ab8b759320c29d0a60795c768048a0197c6260
Sergio <sanchez@ppic.org>
2018-10-16 16:30:41 -0700

    finish rough draft sentences, saved .txt file, make twitterbot v0.0.1

---
+++ src/twitterbot.py
@@ -0,0 +1,27 @@
+import tweepy
+from time import sleep
+from credentials import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+my_file=open('../data/processed/working-sentences.txt','r', encoding='utf-8')
+file_lines=my_file.readlines()
+my_file.close()
+
+# Tweet a line every 15 minutes
+def tweet():
+    for line in file_lines:
+        try:
+             print(line)
+             if line != '\n':
+                 api.update_status(line)
+                 sleep(90)
+             else:
+                pass
+        except tweepy.TweepError as e:
+            print(e.reason)
+            sleep(2)
+
+tweet()
