83b21b0a8c709f8759bebddb58f485a63e6b5213
Sergio <sanchez@ppic.org>
2018-10-19 15:02:56 -0700

    add part 2 notebook and version 2 sentences

--- src/twitterbot.py
+++ src/twitterbot.py
@@ -6,7 +6,11 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+<<<<<<< HEAD
+my_file=open('../data/processed/working-V2-sentences.txt','r', encoding='utf-8')
+=======
 my_file=open('../data/processed/working-sentences.txt','r', encoding='utf-8')
+>>>>>>> db8b3b02a6827c240162b97136e2826a70c5e233
 file_lines=my_file.readlines()
 my_file.close()
 
